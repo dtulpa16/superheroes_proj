342ed8565d5bb853eed9dff0ca96083c91a4fa6f
unknown <dtulpa16@yahoo.com>
2021-08-25 15:15:20 -0500

    added function to search heroes based off id

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from .models import Superhero
+
+def index(request):
+    all_heroes = Superhero.objects.all()
+    context = {
+        'all_heroes' : all_heroes
+    }
+    return render(request, 'superheroes/index.html', context)
+
+def detail(request,hero_id):
+    single_hero = Superhero.objects.get(pk = hero_id)
+    context = {
+        'single_hero': single_hero
+    }
+    return render(request, 'superheroes/detail.html', context)
+
