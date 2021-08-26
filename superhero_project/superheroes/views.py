61ba3b8458546282ff58a14cd757f570af7fd86d
unknown <dtulpa16@yahoo.com>
2021-08-26 12:10:42 -0500

    added update feature

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -32,3 +32,20 @@ def create(request):
         return HttpResponseRedirect(reverse('superheroes:index'))
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request,hero_id):
+    updated_hero = Superhero.objects.get(pk = hero_id)
+    if request.method == 'POST':
+        updated_hero.name = request.POST.get('name')
+        updated_hero.alter_ego = request.POST.get('alter_ego')
+        updated_hero.primary_ability = request.POST.get('primary_ability')
+        updated_hero.secondary_ability = request.POST.get('secondary_ability')
+        updated_hero.catch_phrase = request.POST.get('catch_phrase')
+        updated_hero.save()
+        return HttpResponseRedirect(reverse('superheroes:index'))
+
+    else:
+        context = {
+            'updated_hero': updated_hero
+        }
+        return render(request, 'superheroes/edit.html', context)
